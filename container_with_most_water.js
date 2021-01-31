0bccdfd3caa59693d5ff06dcce1bb435e3855cda
henryhannn <henryhan3996@gmail.com>
2021-01-30 23:35:05 -0800

    add container with most water algo

--- container_with_most_water.js
+++ container_with_most_water.js
@@ -1,3 +1,7 @@
 function mostVolume(arr) {
-  
+  var maxarea = 0;
+  for (var i = 0; i < height.length; i++)
+      for (var j = i + 1; j < height.length; j++)
+          maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
+  return maxarea;
 }
