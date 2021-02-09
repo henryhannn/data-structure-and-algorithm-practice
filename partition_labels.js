6b902bcdb32952841ac46b2fe9943672dd976131
henryhannn <henryhan3996@gmail.com>
2021-02-09 15:58:58 -0800

    add partition label algo

---
+++ partition_labels.js
@@ -0,0 +1,21 @@
+var partitionLabels = function(S) {
+  arr = S.split('')
+  for (let i = 0; i < arr.length; i++) {
+      let start = 0;
+      let max = hash[arr[i]];
+  }
+  // [0, 8]
+  // start = 9
+  // max = 14
+  {
+      a: 8
+      b: 5 // 1
+      c: 7 // 4
+      d: 14 // 9
+      e: 15 // 10
+      f: 11 // 11
+      g: 13 // 13
+      h: 19 // 16
+      i: 22 // 17
+  }
+};
