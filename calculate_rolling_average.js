d3f47e110fec4a97f3647180196415b98504b038
henryhannn <henryhan3996@gmail.com>
2021-02-10 06:20:24 -0800

    add sort helper function and updated mode to hash

--- calculate_rolling_average.js
+++ calculate_rolling_average.js
@@ -2,29 +2,35 @@ class Ledger {
   constructor() {
     // create the array to keep track of all numbers
     this.nums = [];
+    this.sum = 0;
+  }
+
+  // sort helper function
+  sort() {
+    this.nums = this.nums.sort((a, b) => a - b);
   }
 
   // can take in 1 number or multiple numbers
   add(newNum, ...arg) {
     // put the first number into the array
     this.nums.push(newNum);
+    this.sum += newNum;
     // check to see if there are any more inputs
     if (arg.length !== 0) {
       // put the extra inputs into the nums array
       for (let i = 0; i < arg.length; i++) {
         this.nums.push(arg[i]);
+        this.sum += arg[i];
       }
     }
+    // call sort helper function everytime new items are added
+    this.sort();
   }
 
   mean() {
     // dunno if this is necessary but just making sure if there are no inputs, it doesnt return undefined.
     if (this.nums.length === 0) return 0;
-    let sum = 0;
-    for (let i = 0; i < this.nums.length; i++) {
-      sum += this.nums[i];
-    }
-    return sum / this.nums.length;
+    return this.sum / this.nums.length;
   }
 
   median() {
@@ -38,27 +44,37 @@ class Ledger {
   }
 
   mode() {
-    if (this.nums.length === 0) return 0;
-    let counter = 1;
-    let maxCounter = 1;
-    let mode = this.nums[0];
-    // check how many times each number appears
+    // if (this.nums.length === 0) return 0;
+    // let counter = 1;
+    // let maxCounter = 1;
+    // let mode = this.nums[0];
+    // // check how many times each number appears
+    // for (let i = 0; i < this.nums.length; i++) {
+    //   // make sure second iteration doesnt happen if i reaches the end of the array
+    //   if (i !== this.nums.length - 1) {
+    //     for (let j = i + 1; j < this.nums.length; j++) {
+    //       if (this.nums[i] === this.nums[j]) counter += 1;
+    //     }
+    //   }
+    //   // compare the counters and set new mode if necessary
+    //   if (counter > maxCounter) {
+    //     maxCounter = counter;
+    //     mode = this.nums[i];
+    //   }
+    //   // reset counter
+    //   counter = 1;
+    // }
+    // return mode;
+
+    let hash = {};
     for (let i = 0; i < this.nums.length; i++) {
-      // make sure second iteration doesnt happen if i reaches the end of the array
-      if (i !== this.nums.length - 1) {
-        for (let j = i + 1; j < this.nums.length; j++) {
-          if (this.nums[i] === this.nums[j]) counter += 1;
-        }
+      if (hash[this.nums[i]] === undefined) {
+        hash[this.nums[i]] = 0;
+      } else {
+        hash[this.nums[i]] += 1;
       }
-      // compare the counters and set new mode if necessary
-      if (counter > maxCounter) {
-        maxCounter = counter;
-        mode = this.nums[i];
-      }
-      // reset counter
-      counter = 1;
     }
-    return mode;
+    return Object.keys(hash).reduce((a, b) => hash[a] > hash[b] ? a : b);
   }
 }
 
